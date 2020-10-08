5e686bee37289765cd5f84f06d73025132e7581e
Your Name <you@example.com>
2020-10-08 10:42:45 +0800

    Commit from RStudio

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,57 @@
 
 ## Write a short comment describing this function
 
+##  This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  ## Initialize the inverse property
+  i <- NULL
+  
+  ## Set the matrix
+  set <- function( y ) {
+    x <<- y
+    i <<- NULL
+  }
+  
+  ## Get the matrix
+  get <- function() x
+  
+  
+  ## Set the inverse of the matrix
+  setInverse <- function(inverse) {
+    i <<- inverse
+  }
+  
+  ## Get the inverse of the matrix
+  getInverse <- function() i
+  
+  ## Return a list of the methods
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
+#This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  i <- x$getInverse()
+  
+  ## Return the inverse if already in the Cache
+  
+  if( !is.null(i) ) {
+    message("getting cached data")
+    return(i)
+  }  
+  
+  data <- x$get()
+  
+  ## Computing the inverse of a square matrix 
+  i <- solve(data) %*% data
+  
+  x$setInverse(i)
+  
+  i
 }
